dc8a186586a7eae811bf65aa5ec5426edb0c25bf
amatlynd <lyndon.amat@mail.utoronto.ca>
2017-12-21 23:21:48 -0500

    added 24h percent change command

--- bot.js
+++ bot.js
@@ -21,10 +21,12 @@ bot.on('ready', function (evt) {
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
+	var prompt = message.substring(0, 1)
+    if (prompt == '!' ||prompt == '%') {
+		
         var args = message.substring(1).split(' ');
         var cmd = args[0].toUpperCase();
-       
+		var finalMessage = ""
         args = args.splice(1);
 		
 		const url = "https://api.coinmarketcap.com/v1/ticker";
@@ -34,9 +36,15 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 		.then(function(data){
 			for(var i = 0;i < data.length;i++){
 				if(data[i].symbol == cmd){
+					//decides whether the price or percent change is wanted
+					if(prompt == '!'){
+						finalMessage = "$" + data[i].price_usd
+					}if (prompt == '%'){
+						finalMessage = prompt + data[i].percent_change_24h
+					}
 					bot.sendMessage({
 						to: channelID,
-						message: '$'+ data[i].price_usd
+						message: finalMessage
 					});
 					break;
 				}
